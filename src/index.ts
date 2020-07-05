2ae127f2fe83e4d23423a1e0b507d23c33fc518f
theskillwithin <austinppeterson@gmail.com>
2020-07-04 23:02:06 -0600

    nice

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
-import { Node } from "smoke-node";
+import { Node, NetworkHub } from "smoke-node";
 
-const node = new Node();
+// const node = new Node();
+
+const node = new Node({
+  hub: new NetworkHub("wss://smoke--sinclairzx81.repl.co"),
+});
 
 const app = node.rest.createServer();
 
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.send("there is no spoon");
   console.log(res);
 });
 
@@ -15,3 +19,17 @@ node.rest
   .fetch("/")
   .then((response) => response.text())
   .then((text) => console.log(text));
+
+node.address().then((address) => console.log(address));
+
+node.address().then((address) => {
+  node.rest
+    .fetch(`rest://${address}`)
+    .then((res) => res.text())
+    .then((text) => console.log(text));
+});
+
+node.rest
+  .fetch("rest://0.0.0.14")
+  .then((res) => res.text())
+  .then((text) => console.log("sinclairs: ", text));
